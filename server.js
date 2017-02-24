b879681f22972f9dca8c3b9a0b9ed5ced0a7c568
JudyVue <judyvue87@gmail.com>
2017-02-23 23:53:15 -0800

    i hate pure functions

--- server.js
+++ server.js
@@ -12,42 +12,30 @@ const superagent = require('superagent');
 //https://canvas.instructure.com/api/v1/courses/1107581/assignments/5939248/submissions?per_page=1000
 //Use this URL to get ungraded assignments
 
-let startURL = `https://canvas.instructure.com/api/v1/courses/${process.env.COURSE_ID}`
+let discussion = 'discussion_topic'
+let perPage = '?per_page=1000';
+let canvasURL = `https://canvas.instructure.com/api/v1/courses/${process.env.COURSE_ID}`;
 
-let studentsURL = `${startURL}/students`
+let studentsURL = `${canvasURL}/students`
 
-let ungradedLabsURL = `${startURL}/assignments?bucket=ungraded&?per_page=1000`
+let ungradedLabsURL = `${canvasURL}/assignments?bucket=ungraded&${perPage}`
 
+let ghURL = 'https://github.com';
 
-let ghURL = 'https://github.com/codefellows-seattle-javascript-401d14/lab-31-frontend-auth/pull/4';
-
-// {
-//   repoURL:
-//   assnCanvasID:
-//   labName:
-//   studentID:
-// }
-
-// return {
-//   firstName:
-//   lastName:
-//   dirName:
-//   canvasID:
-//   githubUserName:
-//   ungradedLabs: []
-//   ungradedReadings
-// }
 
 let students = [];
+let ungradedLabIDs = [];
 
 const student = (data) => {
   let firstName = data.name.split(' ')[0].trim();
   let lastName = data.name.split(' ')[1].trim();
+  let ungradedLabs = [];
   let studentModel = {
     firstName,
     lastName,
     dirName: `${firstName}-${lastName[0]}`.toLowerCase(),
     canvasID: data.id,
+    ungradedLabs,
   };
   return studentModel;
 };
@@ -66,6 +54,26 @@ superagent.get(studentsURL)
 superagent.get(ungradedLabsURL)
 .set('Authorization', `Bearer ${process.env.CANVAS_TOKEN}`)
 .then(res => {
-  console.log(res.body);
+  return ungradedLabIDs = res.body.map(lab => lab.id);
+})
+.then((ids) => {
+  for (let id of ids){
+    return superagent.get(`${canvasURL}/assignments/${id}/submissions${perPage}`)
+    .set('Authorization', `Bearer ${process.env.CANVAS_TOKEN}`)
+  }
+})
+.then(res => {
+  return res.body.filter(lab => lab.graded_at === null).filter(lab => lab.submission_type !== discussion)
+})
+.then(labs => {
+  let obj = {};
+  for (let student of students){
+    for (let lab of labs){
+      if (student.canvasID === lab.user_id){
+        ungraded.canvasLabID = lab.assignment_id;
+        ungraded.labName =
+      }
+    }
+  }
 })
 .catch(err => console.error(err.message));
