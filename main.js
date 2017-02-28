188e6136c9f74589d280598b538ab5b28e752996
JudyVue <judyvue87@gmail.com>
2017-02-27 23:08:11 -0800

    using post to jaren's grade to prep for lightning talk

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
 'use strict';
 
-require('dotenv').config();
-const debug = require('debug')('grading-main');
 
 //app modules
 const githubCmd = require('./lib/github-command');
-const canvasCmd = require('./lib/canvas-api');
+// const canvasCmd = require('./lib/canvas-api');
 
 const main = () => {
-
-
   Promise.all([
     // fetchCanvasStudents(),
     githubCmd.fetchLabRepoURLs(),
   ]);
-
 };
 main();
