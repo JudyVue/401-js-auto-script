95e1abcb7b27e87587e2d2a49593246a44d156ad
JudyVue <judyvue87@gmail.com>
2017-02-28 21:33:25 -0800

    fixing some mild debug logs

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
 'use strict';
 
 require('dotenv').config();
-const debug = require('debug')('grading-main');
-
 //app modules
 const githubCmd = require('./lib/github-command');
 const canvasCmd = require('./lib/canvas-api');
 
 const main = () => {
-
-
   Promise.all([
     canvasCmd.fetchCanvasStudents(),
     githubCmd.fetchLabRepoURLs(),
   ]);
-
 };
 main();
